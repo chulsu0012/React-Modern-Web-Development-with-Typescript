8149f37edab5e57fc3187fc383bb883e9220da44
chulsu0012 <81545875+chulsu0012@users.noreply.github.com>
2024-01-02 04:28:22 +0900

    10주차

---
+++ ch07_4_server/src/test/deleteTest.ts
@@ -0,0 +1,23 @@
+import * as M from '../mongodb'
+
+const connectCB = async (db: M.MongoDB) => {
+  try {
+    const user = db.collection('user')
+
+    const deleteOneResult = await user.deleteOne({name: {$regex: /^J.*$/}})
+    console.log('deleteOneResult', deleteOneResult)
+
+    const deleteManyResult = await user.deleteMany({name: {$regex: /^J.*$/}})
+    console.log('deleteManyResult', deleteManyResult)
+
+    const deleteAllResult = await user.deleteMany({})
+    console.log('deleteAllResult', deleteAllResult)
+  } catch(e) {
+    if (e instanceof Error) console.log(e.message)
+  }
+}
+const deleteTest = () => {
+  M.connectAndUseDB(connectCB, 'ch07')
+}
+
+deleteTest()
